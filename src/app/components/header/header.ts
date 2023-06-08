214262426a5808658f5fedbaac28ca54940d5768
dndryn <dndryn@outlook.it>
2023-06-08 18:05:58 +0200

    feat: update components in order  to be responsive

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,7 +1,6 @@
 import "./header.scss";
 import create from "@utils/create";
 import { logo } from "@component/logo";
-import { nav_links } from "@component/links";
 import { cta } from "@component/button";
 
 const nav__cta = cta;
@@ -14,7 +13,21 @@ nav__logo.setAttribute("class", "is__flex");
 
 const header = create('header', ["is_flx_rw", "has_x_spcd", "has_y_cntr"]);
 header.id = `app_header`;
-header.append(nav__logo, nav_links, nav__cta);
+
+// Element with dynamic import for mobile menu
+// FIXME: Dynamic import renders element only on first load
+if (window.innerWidth <= 480) {
+  (async () => {
+    const { toggle } = await import("@component/menu");
+    header.append(nav__logo, toggle);
+  })();
+} else {
+  (async () => {
+      //TODO: Put 'cta' too
+      const { nav_links } = await import("@component/links");
+      header.append(nav__logo, nav_links, cta);
+  })();
+}
 
 export { header };
 
--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,8 +1,6 @@
 import './hero.scss';
 import create from "@utils/create";
 import { cta } from "@component/button";
-//import svgImage from '@asset/bg-intro-desktop.svg';
-
 
 const hero_title = create("h1", ["is__flex"]);
 hero_title.setAttribute("id", "hero_title");
@@ -23,15 +21,18 @@ hero_message.append(hero_title, hero_description, hero_cta);
 
 const hero_mockup_image = create("div", ["is_flx"]);
 hero_mockup_image.setAttribute("id", "hero_mockup-image");
-//hero_mockup_image.style.backgroundImage = `url(${svgImage})`;
 
 const hero_container = create("div", ["is__flex", "on__sides"]);
 hero_container.setAttribute("id", "hero_container");
-hero_container.append(hero_message, hero_mockup_image);
+
+// Conditional in order to 
+// improve CSS lightening.
+window.innerWidth <= 480
+  ? hero_container.append(hero_mockup_image, hero_message)
+  : hero_container.append(hero_message, hero_mockup_image);
 
 const hero = create('section', ["is__flex"], hero_container);
 hero.setAttribute("id", "hero");
-
 
 export { hero };
 
